9d359f7d8b419774fd4a51f5a0622a790b772059
Corey Martin <coreyjmartin2@gmail.com>
2020-08-20 22:25:02 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,10 +1,7 @@
-jewels_in_bag = 100
+
 
 7.times do
-  puts ""Hiding 10 stolen jewels.""
-  jewels_in_bag = jewels_in_bag - 10
+  puts "Wingardium Leviosa"
 end
-
-puts "We have #{jewels_in_bag} jewels to hide!"
 
 
